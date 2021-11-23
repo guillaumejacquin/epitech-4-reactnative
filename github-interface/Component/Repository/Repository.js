4d5a5bbb2e9fc069d6c6d2e39ffe564b144e051f
raphaeljaoui <raphael.jaoui@epitech.eu>
2021-11-23 17:39:01 +0100

    [UP]component

---
+++ github-interface/Component/Repository/Repository.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Repository = () => {
+    return (
+        <View>
+            <Text>Repo</Text>
+        </View>
+    )
+}
+
+export default Repository
+
+const styles = StyleSheet.create({})
