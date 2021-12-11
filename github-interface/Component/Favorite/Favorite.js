a8a854a365ce1064bcd6826e8a5ad74101235dd8
Lina <lina1.kaci@epitech.eu>
2021-12-11 09:34:04 +0100

    add the unstar button that delete the repo from the starred

--- github-interface/Component/Favorite/Favorite.js
+++ github-interface/Component/Favorite/Favorite.js
@@ -1,48 +1,74 @@
 import React, {useEffect, useState} from 'react'
-import { StyleSheet, Text, View, SafeAreaView, ScrollView } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, ScrollView, TouchableOpacity} from 'react-native'
 import { connect } from 'react-redux'
-import Card from "../Home/Card"
+import Search from '../Search/Search';
+import { Button } from 'react-native-elements';
+import { Icon } from 'react-native-elements';
 
 const Favorite = ({octokit, navigation}) => {
-    const [user, setFavorites] = useState([])
+    const [favorites, setFavorites] = useState([{}])
+    const [deleteStar, setDelete] = useState();
+    
     const getData = async() => {
-        const {data} = await octokit.request("GET /user/repos");
+        const { data } = await octokit.request("GET /user/starred");
         return data
     }
+    const deleteD = async(favorite) => {
+        octokit.rest.activity.unstarRepoForAuthenticatedUser({
+            owner: favorite.owner.login,
+            repo: favorite.name,
+        }).then(res => {
+            console.log(res);
+            setDelete()
+        });
+    }
+
     useEffect(() => {
         getData().then(res => {
             setFavorites(res);
-        })
+        });
     }, [octokit])
-    const favoritesExists = () => {
-        console.log("teeeeeeeeeeeeest")
-        if (user) { 
-           return <Text style={styles.subTitle}>
-                {user.login}
-            </Text>
-        } else {
-            return <Text style={styles.subTitle}>
-            REPOSITORIES
-        </Text>
-        }
-    }
+
+
     return (
         <SafeAreaView style={{flex: 1, backgroundColor: "white"}}>
           <ScrollView>
               <View style={{flexDirection: "column", marginVertical: 10}}>
-
                     <Text style={styles.title}>
-                        Overview
+                        <Icon name='star' /> Favoris
                     </Text>
-                    {favoritesExists()}
-                  <Card title={"Repositories"} navigation={navigation} navigate={"Repositories"}/>
-                  <Card title={"Projects"} navigation={navigation} navigate={"Issues"}/>
-                  <Card title={"Packages"} navigation={navigation} navigate={"PullRequests"}/>
+                    {favorites.map(favorite => (
+                        <TouchableOpacity onPress={() => console.log("lol")}>
+                            <Text style={styles.cardTitle}>
+                                <View style={styles.statBar}>
+                                    <Text style={styles.cardTitle}>
+                                        {favorite.name}
+                                    </Text>
+                                    <Button
+                                        onPress={() => {
+                                            const oldFavorite = favorite;
+                                            if (favorite) {
+                                                deleteD(favorite)
+                                                .then(res => console.log(res))
+                                                .catch(error =>
+                                                    console.log("An error occured :", error)
+                                                );
+                                            } 
+                                        }}    
+                                            icon={<Icon name="star" color="gray"/>}
+                                            title="Starred"
+                                            type="outline"
+                                    />
+                                    </View>
+                                </Text>
+                            </TouchableOpacity>
+                  ))
+                }
               </View>
           </ScrollView>
         </SafeAreaView>
     )
-}
+            }   
 
 const mapStateToProps = state => state;
 const connectComponent = connect(mapStateToProps, undefined)
@@ -50,6 +76,34 @@ const connectComponent = connect(mapStateToProps, undefined)
 export default connectComponent(Favorite)
 
 const styles = StyleSheet.create({
+    statBar: {
+        alignItems: "center",
+        flexDirection: "row",
+        marginHorizontal: 10,
+        marginVertical: 7,
+        backgroundColor: "white",
+        justifyContent: "space-between",
+        paddingVertical: 12,
+        borderRadius: 10,
+        shadowRadius: 10,
+        shadowColor: 'black',
+        shadowOpacity: 0.2,
+        shadowOffset: {
+            width: 5,
+            height: 5}
+        },
+    cardTitle: {
+        fontSize: 20,
+        fontWeight: "700",
+        marginLeft: 20,
+        marginVertical: 4,
+        marginRight: 20
+    },
+    texte: {
+        fontSize: 20,
+        fontWeight: "600",
+        marginLeft: 50
+    },
     title: {
         fontSize: 30,
         fontWeight: "700",
@@ -59,5 +113,5 @@ const styles = StyleSheet.create({
         fontSize: 20,
         fontWeight: "700",
         padding: 30
-    },
+    }
 })
