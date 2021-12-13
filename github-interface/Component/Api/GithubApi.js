8cdad1d2dbc7541e5424ee0e99626fecef0c7209
guiguibg <guillaume.jacquin@epitech.eu>
2021-12-13 20:50:16 +0100

    [add]

--- github-interface/Component/Api/GithubApi.js
+++ github-interface/Component/Api/GithubApi.js
@@ -3,7 +3,7 @@ import { DrawerActions } from "@react-navigation/routers";
 
 export const auth_github= async() => {
     const octokit = new Octokit({
-        auth: "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",
+        auth: "",
       });
       // const { orgs } = await octokit.request('GET /organizations').then(orgs => {
       //   console.log(orgs.data[3].login)
